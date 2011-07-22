ea51731222540fe236f8b81cbf517f81034bcf9b
Yonatan Graber <yonatan.graber@gmail.com>
2011-07-22 19:54:20 +0300

    Changed the hack mechanizm to patch the appengine-tools-api.jar, by adding the custom wrapper into it

--- src/me/yonatan/gwp/server/GaeJarDropWizard.java
+++ src/me/yonatan/gwp/server/GaeJarDropWizard.java
@@ -1,15 +1,20 @@
 package me.yonatan.gwp.server;
 
-import java.io.ByteArrayInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.jar.JarOutputStream;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
@@ -21,7 +26,8 @@ import org.eclipse.wst.server.core.TaskModel;
 @SuppressWarnings("restriction")
 public class GaeJarDropWizard extends GenericServerRuntimeWizardFragment {
 
-	public static final String jarName = "appengine-wrapper.jar";
+	public static final String sourceJarName = "appengine-tools-api.jar";
+
 	private static Logger log = Logger.getLogger(GaeJarDropWizard.class
 			.getName());
 
@@ -37,15 +43,26 @@ public class GaeJarDropWizard extends GenericServerRuntimeWizardFragment {
 		String serverRootDirectory = gsr.getServerInstanceProperties()
 				.get("serverRootDirectory").toString();
 
-		File file = new File(serverRootDirectory + File.separatorChar + "lib"
-				+ File.separatorChar + jarName);
-		file.mkdirs();
-		if (file.exists()) {
-			file.delete();
-		}
+		String sourceFilename = serverRootDirectory + File.separatorChar
+				+ "lib" + File.separatorChar + sourceJarName;
+		String targetFilename = FilenameUtils.removeExtension(sourceFilename)
+				+ ".tmp";
+		String backupFilename = FilenameUtils.removeExtension(sourceFilename)
+				+ ".bak";
+		File sourceFile = new File(sourceFilename);
+
+		File targetFile = new File(targetFilename);
 
 		try {
-			writeRuntimeJar(file);
+			FileUtils.forceDelete(targetFile);
+			writeRuntimeJar(sourceFile, targetFile);
+
+			if (!new File(backupFilename).exists()) {
+				sourceFile.renameTo(new File(backupFilename));
+			} else {
+				sourceFile.delete();
+			}
+			targetFile.renameTo(new File(sourceFilename));
 		} catch (Exception e) {
 			log.severe("Exception " + e.getMessage());
 		}
@@ -53,39 +70,63 @@ public class GaeJarDropWizard extends GenericServerRuntimeWizardFragment {
 		log.info(gsr.getServerInstanceProperties().toString());
 	}
 
-	private void writeRuntimeJar(File outFile) throws Exception {
+	private void writeRuntimeJar(File originalFile, File outputFile)
+			throws Exception {
+		JarFile sourceJar = new JarFile(originalFile);
+		JarOutputStream targetJar = new JarOutputStream(
+				new BufferedOutputStream(new FileOutputStream(outputFile)));
+		// TODO write entries as STORED
+		// targetJar.setMethod(JarEntry.STORED);
+		try {
+
+			copyJar(sourceJar, targetJar);
+			writeHackClasses(targetJar);
+		} finally {
+			try {
+				sourceJar.close();
+			} catch (Exception e) {
+			}
+			try {
+				targetJar.close();
+			} catch (Exception e) {
+			}
+
+		}
+	}
+
+	private void writeHackClasses(JarOutputStream targetJar) throws IOException {
+
 		String pack = "me/yonatan/gwp/server/";
 		Class<?>[] classes = new Class[] { AdminServer.class, Runner.class,
 				Stopper.class, Runner.ShutdownHook.class };
 		String[] names = new String[] { pack + "AdminServer.class",
 				pack + "Runner.class", pack + "Stopper.class",
 				pack + "Runner$ShutdownHook.class" };
-		OutputStream out = new FileOutputStream(outFile);
-		JarOutputStream jar = new JarOutputStream(out);
 		for (int i = 0; i < names.length; i++) {
 			InputStream in = classes[i].getResourceAsStream("/" + names[i]);
 			System.out.println(names[i] + "  -  " + in + "\n\n");
-			writeIntoJar(jar, names[i], in);
+			targetJar.putNextEntry(new ZipEntry(names[i]));
+			IOUtils.copy(in, targetJar);
+			targetJar.closeEntry();
+			in.close();
 		}
-		ByteArrayInputStream bis = new ByteArrayInputStream(
-				"Manifest-Version: 1.0\n".getBytes());
-		writeIntoJar(jar, "META-INF/MANIFEST.MF", bis);
-		jar.close();
-		out.close();
-
 	}
 
-	private void writeIntoJar(JarOutputStream jar, String depName,
-			InputStream in) throws IOException {
-		jar.putNextEntry(new ZipEntry(depName));
-		byte[] b = new byte[65536];
-		while (true) {
-			int r = in.read(b);
-			if (r == -1)
-				break;
-			jar.write(b, 0, r);
+	private void copyJar(JarFile sourceJar, JarOutputStream targetJar)
+			throws IOException {
+		Enumeration<JarEntry> entires = sourceJar.entries();
+
+		while (entires.hasMoreElements()) {
+			JarEntry entry = entires.nextElement();
+			if (entry.getName().startsWith("me/yonatan/"))
+				continue;
+			JarEntry targetEntry = new JarEntry(entry.getName());
+
+			targetJar.putNextEntry(targetEntry);
+			IOUtils.copy(sourceJar.getInputStream(entry), targetJar);
+			targetJar.closeEntry();
+
 		}
-		jar.closeEntry();
-		in.close();
+
 	}
 }
