e30226e8c3223aa35e3398ec00b8c2b32dcce76c
Yonatan Graber <yonatan.graber@gmail.com>
2011-07-30 22:30:10 +0300

    Added section to UI

---
+++ src/me/yonatan/gwp/ui/editor/WorkingDirectorySection.java
@@ -0,0 +1,12 @@
+package me.yonatan.gwp.ui.editor;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.wst.server.ui.editor.ServerEditorSection;
+//import org.eclipse.ui.forms.widgets.FormToolkit;
+
+public class WorkingDirectorySection extends ServerEditorSection {
+	@Override
+	public void createSection(Composite parent) {
+		super.createSection(parent);
+	}
+}
