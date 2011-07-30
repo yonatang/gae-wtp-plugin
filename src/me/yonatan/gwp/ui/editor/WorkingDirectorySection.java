6b835e9c31e86607f2eb3a815fc6638f211f16ad
Yonatan Graber <yonatan.graber@gmail.com>
2011-07-30 23:02:10 +0300

    Added forms dependency

--- src/me/yonatan/gwp/ui/editor/WorkingDirectorySection.java
+++ src/me/yonatan/gwp/ui/editor/WorkingDirectorySection.java
@@ -1,12 +1,23 @@
 package me.yonatan.gwp.ui.editor;
 
+//import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.wst.server.ui.editor.ServerEditorSection;
+//import org.eclipse.ui.forms.widgets.ExpandableComposite;
 //import org.eclipse.ui.forms.widgets.FormToolkit;
+//import org.eclipse.ui.forms.widgets.Section;
+import org.eclipse.wst.server.ui.editor.ServerEditorSection;
 
 public class WorkingDirectorySection extends ServerEditorSection {
 	@Override
 	public void createSection(Composite parent) {
 		super.createSection(parent);
+		System.out.println("Section");
+		
+//		FormToolkit toolkit = new FormToolkit(parent.getDisplay());
+//		Section section = toolkit.createSection(parent, ExpandableComposite.TWISTIE | ExpandableComposite.EXPANDED
+//				| ExpandableComposite.TITLE_BAR);
+//		section.setText("Working directory");
+//		section.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_FILL));
+
 	}
 }
