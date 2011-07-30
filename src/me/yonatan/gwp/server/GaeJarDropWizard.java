53cb319ff13a6811e3bfd57397773bf2e44b4f83
Yonatan Graber <yonatan.graber@gmail.com>
2011-07-30 21:52:43 +0300

    Fixed issue 7 Eliminate the need to hack a jar

--- src/me/yonatan/gwp/server/GaeJarDropWizard.java
+++ src/me/yonatan/gwp/server/GaeJarDropWizard.java
@@ -5,15 +5,11 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 import java.util.jar.JarOutputStream;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -25,6 +21,9 @@ import org.eclipse.wst.server.core.TaskModel;
 
 @SuppressWarnings("restriction")
 public class GaeJarDropWizard extends GenericServerRuntimeWizardFragment {
+
+	public static final String APPENGINE_ADMIN_SERVER_NAME = "lib"
+			+ File.separator + "appengine-admin-server.jar";
 
 	public static final String sourceJarName = "appengine-tools-api.jar";
 
@@ -43,50 +42,27 @@ public class GaeJarDropWizard extends GenericServerRuntimeWizardFragment {
 		String serverRootDirectory = gsr.getServerInstanceProperties()
 				.get("serverRootDirectory").toString();
 
-		String sourceFilename = serverRootDirectory + File.separatorChar
-				+ "lib" + File.separatorChar + sourceJarName;
-		String targetFilename = FilenameUtils.removeExtension(sourceFilename)
-				+ ".tmp";
-		String backupFilename = FilenameUtils.removeExtension(sourceFilename)
-				+ ".bak";
-		File sourceFile = new File(sourceFilename);
+		String targetJarFilename = serverRootDirectory + File.separator
+				+ APPENGINE_ADMIN_SERVER_NAME;
 
-		File targetFile = new File(targetFilename);
+		File targetFile = new File(targetJarFilename);
 
 		try {
 			FileUtils.deleteQuietly(targetFile);
-			writeRuntimeJar(sourceFile, targetFile);
-
-			if (!new File(backupFilename).exists()) {
-				sourceFile.renameTo(new File(backupFilename));
-			} else {
-				sourceFile.delete();
-			}
-			targetFile.renameTo(new File(sourceFilename));
+			writeRuntimeJar(targetFile);
 		} catch (Exception e) {
 			log.severe("Exception " + e.getMessage());
 		}
 
-		log.info(gsr.getServerInstanceProperties().toString());
+		log.info("performFinish done");
 	}
 
-	private void writeRuntimeJar(File originalFile, File outputFile)
-			throws Exception {
-		JarFile sourceJar = new JarFile(originalFile);
+	private void writeRuntimeJar(File outputFile) throws Exception {
 		JarOutputStream targetJar = new JarOutputStream(
 				new BufferedOutputStream(new FileOutputStream(outputFile)));
-		// TODO write entries as STORED
-		// targetJar.setMethod(JarEntry.STORED);
 		try {
-
-			copyJar(sourceJar, targetJar);
 			writeHackClasses(targetJar);
 		} finally {
-			try {
-				sourceJar.close();
-			} catch (Exception e) {
-				log.severe("Can't close source file " + e.getMessage());
-			}
 			try {
 				targetJar.close();
 			} catch (Exception e) {
@@ -98,34 +74,21 @@ public class GaeJarDropWizard extends GenericServerRuntimeWizardFragment {
 
 	private void writeHackClasses(JarOutputStream targetJar) throws IOException {
 
-		String pack = "me/yonatan/gwp/server/";
 		Class<?>[] classes = new Class[] { AdminServer.class, Runner.class,
 				Stopper.class, Runner.ShutdownHook.class };
-		String[] names = new String[] { pack + "AdminServer.class",
-				pack + "Runner.class", pack + "Stopper.class",
-				pack + "Runner$ShutdownHook.class" };
+		String[] names = new String[] { "AdminServer.class", "Runner.class",
+				"Stopper.class", "Runner$ShutdownHook.class" };
 		for (int i = 0; i < names.length; i++) {
-			InputStream in = classes[i].getResourceAsStream("/" + names[i]);
-			targetJar.putNextEntry(new ZipEntry(names[i]));
+			Class<?> clazz = classes[i];
+			String pack = clazz.getPackage().getName().replace('.', '/') + "/";
+			String name = pack + names[i];
+
+			InputStream in = clazz.getResourceAsStream("/" + name);
+			targetJar.putNextEntry(new ZipEntry(name));
 			IOUtils.copy(in, targetJar);
 			targetJar.closeEntry();
 			in.close();
 		}
 	}
 
-	private void copyJar(JarFile sourceJar, JarOutputStream targetJar)
-			throws IOException {
-		Enumeration<JarEntry> entires = sourceJar.entries();
-
-		while (entires.hasMoreElements()) {
-			JarEntry entry = entires.nextElement();
-			if (entry.getName().startsWith("me/yonatan/"))
-				continue;
-			JarEntry targetEntry = new JarEntry(entry.getName());
-
-			targetJar.putNextEntry(targetEntry);
-			IOUtils.copy(sourceJar.getInputStream(entry), targetJar);
-			targetJar.closeEntry();
-		}
-	}
 }
